d7fbd6fc728f8cf75726e689ab32eb2e99a0f64e
kdemello1980 <k6demello28@fastmail.fm>
2020-09-06 13:43:47 -0400

    commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,49 @@
+/**
+ * Modules.
+ */
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+/**
+ * Components.
+ */
 import { AppComponent } from './app.component';
+import { ResumeComponent } from './resume/resume.component';
+import { AboutMeComponent } from './about-me/about-me.component';
+import { BioComponent } from './bio/bio.component';
+
+/**
+ * Material.
+ */
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MatCardModule } from '@angular/material/card';
+import {MatGridListModule} from '@angular/material/grid-list';
+import {MatExpansionModule} from '@angular/material/expansion';
+import { MatTableModule } from '@angular/material/table';
+import {MatIconModule} from '@angular/material/icon';
+import {MatToolbarModule} from '@angular/material/toolbar';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ResumeComponent,
+    AboutMeComponent,
+    BioComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    FlexLayoutModule,
+    MatCardModule,
+    MatGridListModule,
+    MatExpansionModule,
+    MatTableModule,
+    MatIconModule,
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
